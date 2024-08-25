80652418d7ec3c6e6ad4bb109aa5349b1bb43e91
J4cKr0y <Kiky270281@gmail.com>
2024-08-25 21:55:12 +0200

    chore: MongoDB connect

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mon vieux Grimoire
+
+
+## Comment lancer le projet ? 
+- Télécharchez le FrontEnd de la manière de votre choix, et lisez le readme pour savoir comment le lancer.
+Pour le télécharger, c'est ici : https://github.com/OpenClassrooms-Student-Center/P7-Dev-Web-livres
+- Téléchargez le BackEnd (si vous lisez ceci, vous êtes au bon endroit)
+Si vous voulez néanmoins le lien, le voici : https://github.com/J4cKr0y/Mon-Vieux-Grimoire
+
+
+### Avec npm
+Allez dans le dossier où se situe de BackEnd, et lancez : `nodemon server`.
+Ensuite, suivez le readme du FrontEnd pour le lancer. (`npm start`).
+
+Bonne visite ! :-)
--- app.js
+++ app.js
@@ -1,24 +1,87 @@
 const express = require('express');
-//Cette application Express contient quatre éléments de middleware
 const app = express();
-//le premier enregistre « Requête reçue ! » dans la console et passe l'exécution
+const mongoose = require('mongoose');
+const z_mongodb = require('./z_mongodb.js');
+app.use(express.json());
+
+mongoose.connect(z_mongodb.mongodbURI,{})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//Middleware ajoutant les header pour éviter l'erreur CORS (Cross Origin Resource Sharing) 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+	//accéder à notre API depuis n'importe quelle origine
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    //ajoute les headers mentionnés aux requêtes envoyées vers notre API
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    //envoyer des requêtes avec les méthodes mentionnées
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
-//le deuxième ajoute un code d'état 201 à la réponse et passe l'exécution 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+
+//Middleware POST books
+app.post('/api/books', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
 });
-//le troisième envoie la réponse JSON et passe l'exécution
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+
+app.get('/api/users', (req, res, next) => {
+  const users = [
+    {
+	  email: 'sophie.bluel@test.tld',
+	  password: 'S0phie',
+	  userId: 'a1b2c3d4e5f6789012345678',
+	  token: '',
+	},
+	{
+	  email: 'nomail@yopmail.com',
+	  password: 'C7ejsUidAGD6NCct',
+	  userId: '60d5f9e8c9a1b2001a2e4f3c',
+	  token: '',
+	}
+  ];
+  res.status(300).json(users);
 });
-//le dernier élément de middleware enregistre « Réponse envoyée avec succès ! » dans la console.
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+
+
+//Middleware GET books
+app.get('/api/books', (req, res, next) => {
+  const books = [
+    {
+	  userId: 'a1b2c3d4e5f6789012345678',
+      title: 'L`hisoire sans fin',
+	  author: 'Michael Ende',
+      imageUrl: 'https://f.media-amazon.com/images/I/71FXt0UoAlL._SY466_.jpg',
+	  year: '2014',
+	  genre: 'Jeunesse',
+	  ratings : [
+		{
+		userId : '5f47ac10b58c1e001c8d4b6a',
+		grade : '5',
+		}
+	  ],
+	  averageRating : '5',
+    },
+    {
+      userId: '60d5f9e8c9a1b2001a2e4f3c',
+      title: 'Second Oekumene, tome 4: Vatican',
+	  author: 'John Crossford',
+	  imageUrl: 'https://f.media-amazon.com/images/I/71RaAJ27WIL._SY466_.jpg',
+	  year: '2023',
+	  genre: 'Science-fiction',
+	  ratings : [
+		{
+		userId : 'b2c3d4e5f678901234567890',
+		grade : '4.8',
+		}
+	  ],
+	  averageRating : '4.5',
+    },
+  ];
+  res.status(200).json(books);
 });
 
 module.exports = app;
